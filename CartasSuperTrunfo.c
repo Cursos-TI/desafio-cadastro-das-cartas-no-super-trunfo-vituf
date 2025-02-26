f56646c8cd1c53612fed36babdeddc5c9a9b7890
vituf <vitoremanuelrs5@gmail.com>
2025-02-26 13:01:45 +0000

    Arrumando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,10 @@ int main() {
     float area, pib;
     int pontos_turisticos, populacao;
 
-   // funcões para a entrada de dados 
+   // funcões pra entrada das informações da primeira carta
+
+    printf("Carata 1:\n\n");
+
     printf("Estado: ");
     scanf("%s", estado);
 
@@ -30,15 +33,52 @@ int main() {
     printf("Numeros de pontos turísticos: ");
     scanf("%d", &pontos_turisticos);
 
-    system ("clear");
+    system("clear");
 
-   // funçôes para a saida de dados 
+   // funçôes para a saida das informações da Carta 1
+
     printf("Informações da Cidade:\n");
+    printf("Carta 1:\n\n"); 
     printf("-Código da Carta: %s\n", codigo);
     printf("-Nome da Cidade: %s\n", cidade);
     printf("-População: %d\n", populacao);
     printf("-Área: %.3f km²\n", area);
-    printf("-PIB: %.3f bilhões de reais\n", pib);
+    printf("-PIB: %.1f bilhões de reais\n", pib);
+    printf("-Numero de pontos turísticos: %d\n", pontos_turisticos);
+
+    // funcões pra entrada das informações da primeira carta
+    printf("Carata 2:\n\n");
+
+    printf("Estado: ");
+    scanf("%s", estado);
+
+    printf("Código da Carta: ");
+    scanf("%s", codigo);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", cidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Área: ");
+    scanf("%f", &area);
+
+    printf("PIB: ");
+    scanf("%f", &pib);
+
+    printf("Numeros de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+
+
+    // funçôes para a saida das informações da Carta
+
+    printf("Carta 2:\n\n");
+    printf("-Código da Carta: %s\n", codigo);
+    printf("-Nome da Cidade: %s\n", cidade);
+    printf("-População: %d\n", populacao);
+    printf("-Área: %.3f km²\n", area);
+    printf("-PIB: %.1f bilhões de reais\n", pib);
     printf("-Numero de pontos turísticos: %d\n", pontos_turisticos);
 
     return 0;
--- Super_trunfo_Per Capita e densidade.c
+++ Super_trunfo_Per Capita e densidade.c
@@ -9,7 +9,9 @@ int main() {
     float densiade, per_capita;
     int pontos_turisticos, populacao;
 
-   // funcões de entrada de dados 
+   // funcões de entrada das informações da Carta 1
+    printf("Carta 1:\n\n");
+
     printf("Estado: ");
     scanf("%s", estado);
 
@@ -31,15 +33,58 @@ int main() {
     printf("Numeros de pontos turísticos: ");
     scanf("%d", &pontos_turisticos);
 
-    system ("clear");
-
    //operacões de densidade e PIB per Capita
    densiade = (float)populacao /  area;
 
    per_capita = (float)populacao / pib;
 
-   // funçôes de saida de dados 
-    printf("Informações da Cidade:\n");
+   // funçôes de saida das informações da Carta 1
+    printf("Carta 1:\n");
+    printf("EStado: %s\n", estado);
+    printf("-Código da Carta: %s\n", codigo);
+    printf("-Nome da Cidade: %s\n", cidade);
+    printf("-População: %d\n", populacao);
+    printf("-Área: %.3f km²\n", area);
+    printf("-PIB: %.0f bilhões de reais\n", pib);
+    printf("-Numero de pontos turísticos: %d\n", pontos_turisticos);
+    printf("-Densidade Populacional: %.3f\n", densiade);
+    printf("-PIB per Capita: %.3f\n", per_capita);
+
+
+    // funcões de das informações da Carta 2
+    printf("Carta 2:\n\n");
+
+    printf("Estado: ");
+    scanf("%s", estado);
+   
+    printf("Código da Carta: ");
+    scanf("%s", codigo);
+   
+    printf("Nome da Cidade: ");
+    scanf("%s", cidade);
+   
+    printf("População: ");
+    scanf("%d", &populacao);
+   
+    printf("Área: ");
+    scanf("%f", &area);
+   
+    printf("PIB: ");
+    scanf("%f", &pib);
+
+    printf("Numeros de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+   
+   
+    //operacões de densidade e PIB per Capita
+    densiade = (float)populacao /  area;
+   
+    per_capita = (float)populacao / pib;
+   
+    // funçôes de saida das informações da Carta 2
+
+    printf("Carta 2:\n");
+    printf("EStado: %s\n", estado);
     printf("-Código da Carta: %s\n", codigo);
     printf("-Nome da Cidade: %s\n", cidade);
     printf("-População: %d\n", populacao);
